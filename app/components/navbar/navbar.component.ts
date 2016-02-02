feb7d5f2cdbaa3adfa4406fb3e648f0743d416f2
Pierre-Henri <pierre-henri.bourdeau@brandalley.com>
2016-02-02 16:31:32 +0100

    Work so far

---
+++ app/components/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import {Component, View} from 'angular2/core';
+
+@Component({
+  selector: 'nav-bar',
+})
+@View({
+    templateUrl: './app/components/navbar/navbar.component.html',
+})
+
+export class NavbarComponent {
+
+}
