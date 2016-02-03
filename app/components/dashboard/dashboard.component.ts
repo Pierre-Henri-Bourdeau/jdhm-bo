6ade35eb47b961bdcc4322219c86091034b0405f
Pierre-Henri <pierre-henri.bourdeau@brandalley.com>
2016-02-03 12:35:31 +0100

    Removed Hero sample

--- app/components/dashboard/dashboard.component.ts
+++ app/components/dashboard/dashboard.component.ts
@@ -1,39 +1,24 @@
-import {Component, OnInit, View} from 'angular2/core';
-import {Hero} from '../../services/hero/hero';
-import {HeroDetailComponent} from '../hero/hero-detail.component';
-import {HeroService} from '../../services/hero/hero.service';
+import {Component, View} from 'angular2/core';
 import {NavbarComponent} from '../navbar/navbar.component';
 import {SidebarComponent} from '../sidebar/sidebar.component';
+import {ClientComponent} from '../client/client.component';
+
 
 @Component({
   selector: 'my-app',
-  providers: [HeroService]
 })
 @View({
     templateUrl: './app/components/dashboard/dashboard.component.html',
     styleUrls: ['./app/components/dashboard/dashboard.component.css'],
-    directives: [HeroDetailComponent, NavbarComponent, SidebarComponent],
+    directives: [NavbarComponent, SidebarComponent],
 })
 
-export class DashboardComponent implements OnInit {
 
-  public title = 'Tour of Heroes';
-  public heroes: Hero[];
-  public selectedHero: Hero;
+export class DashboardComponent {
 
-  constructor(private _heroService: HeroService) {
+  constructor() {
 
   }
 
-  getHeroes() {
-    this._heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
 
-  ngOnInit() {
-    this.getHeroes();
-  }
-
-  onSelect(hero: Hero) {
-      this.selectedHero = hero;
-  }
 }
