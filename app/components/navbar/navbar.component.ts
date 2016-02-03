ae6274292f4fc4ac2bc4e27462baeca40f96c848
Pierre-Henri <pierre-henri.bourdeau@brandalley.com>
2016-02-03 15:29:51 +0100

    Added router to navbar

--- app/components/navbar/navbar.component.ts
+++ app/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import {Component, View} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
-  selector: 'jdhm-nav-bar',
+    selector: 'jdhm-nav-bar',
 })
 @View({
   templateUrl: './app/components/navbar/navbar.component.html',
+  directives: [ROUTER_DIRECTIVES],
 
 })
 export class NavbarComponent {
-
 }
