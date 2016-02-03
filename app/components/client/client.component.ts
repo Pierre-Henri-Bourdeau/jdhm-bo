9fdf4071fd331b4a26faf2fc210a7c736d6eaa20
Pierre-Henri <pierre-henri.bourdeau@brandalley.com>
2016-02-03 12:22:10 +0100

    WIP

---
+++ app/components/client/client.component.ts
@@ -0,0 +1,12 @@
+import {Component, View} from 'angular2/core';
+
+@Component({
+  selector: 'jdhm-main-container',
+})
+@View({
+  templateUrl: './app/components/client/client.component.html',
+
+})
+export class ClientComponent {
+
+}
--- app/components/sidebar/sidebar.component.ts
+++ app/components/sidebar/sidebar.component.ts
@@ -5,8 +5,13 @@ import {Component, View} from 'angular2/core';
 })
 @View({
   templateUrl: './app/components/sidebar/sidebar.component.html',
+  styleUrls: ['./app/components/dashboard/dashboard.component.css'],
 
 })
 export class SidebarComponent {
+
+    clientClicked(event) {
+        console.log("You clicked on client!");
+    }
 
 }
