f1f05944fd25cff2d8fe76dbd8a7f244e8caa608
Pierre-Henri <pierre-henri.bourdeau@brandalley.com>
2016-02-10 17:45:49 +0100

    Map

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,8 @@
-import {bootstrap}    from 'angular2/platform/browser'
-import {MainComponent} from './components/main/main.component'
+import {bootstrap}    from 'angular2/platform/browser';
+import {MainComponent} from './components/main/main.component';
 import {ROUTER_PROVIDERS} from 'angular2/router';
-import {HTTP_PROVIDERS} from 'angular2/http'
+import {HTTP_PROVIDERS} from 'angular2/http';
+import 'rxjs/Rx'
 
 bootstrap(MainComponent, [
     ROUTER_PROVIDERS,
--- app/services/api/api.service.ts
+++ app/services/api/api.service.ts
@@ -1,33 +1,41 @@
 import {Injectable} from 'angular2/core';
-import {Http, Headers, HTTP_PROVIDERS} from 'angular2/http'
+import {Http, Response, Headers, HTTP_PROVIDERS} from 'angular2/http'
+import {Observable} from 'rxjs/Observable';
 import {Client} from '../../interfaces/client.interface';
 
 @Injectable()
 
 export class ApiService {
 
-  private http: Http;
-  private baseURL: string;
+    private http: Http;
+    private baseURL: string;
 
-  constructor(http: Http) {
-      this.http = http;
-      this.baseURL = 'http://jdhm-api/app_dev.php';
-  }
+    constructor(http: Http) {
+        this.http = http;
+        this.baseURL = 'http://jdhm-api/app_dev.php';
+    }
 
-  callAPI(url:string): any {
-      return this.http.get(this.baseURL + url);
-  }
+    callAPI(url: string): any {
+        return this.http.get(this.baseURL + url);
+    }
 
-  postAPI(url: string, data) {
-      return this.http.post(this.baseURL + url, JSON.stringify(data))
-  }
+    postAPI(url: string, data) {
+        return this.http.post(this.baseURL + url, JSON.stringify(data))
+    }
 
-  getClients() {
-      return this.callAPI('/clients');
-  }
+    getClients() {
+        return this.callAPI('/clients');
+    }
 
-  updateClient(client: Client) {
-      return this.postAPI('/client', client);
-  }
+    updateClient(client: Client) {
+        return this.postAPI('/client', client)
+                   .map(res => res.json())
+                   .catch(this.handleError);
+    }
+
+    private handleError(error: Response) {
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
+    }
 
 }
