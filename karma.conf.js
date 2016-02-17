2ee0ecb260f4b332fffc6c2ea0b55665e990daf8
Pierre-Henri <phbasic@gmail.com>
2016-02-17 12:57:43 +0100

    Added preprocessor typescript

--- karma.conf.js
+++ karma.conf.js
@@ -2,87 +2,104 @@
 // Generated on Wed Feb 17 2016 10:45:47 GMT+0100 (CET)
 
 module.exports = function(config) {
-  config.set({
+    config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: './',
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: './',
 
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['systemjs', 'jasmine'],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-      'app/**/*spec.js'
-    ],
-
-    systemjs: {
-
-      // Point out where the SystemJS config file is
-      configFile: 'app/system.conf.js',
-
-      serveFiles: [
-        'app/**/*.js'
-      ]
-    },
-
-    plugins: [
-      'karma-systemjs',
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-phantomjs-launcher',
-      'karma-jasmine',
-      'karma-junit-reporter',
-      'karma-coverage'
-    ],
-
-    // list of files to exclude
-    exclude: [
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['systemjs', 'jasmine'],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        // list of files / patterns to load in the browser
+        files: [
+            'app/**/*spec.js'
+        ],
+
+        systemjs: {
+            // Point out where the SystemJS config file is
+            configFile: 'app/system.conf.js',
+            serveFiles: [
+                'app/**/*.js'
+            ]
+        },
+
+        plugins: [
+            'karma-systemjs',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-phantomjs-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter',
+            'karma-coverage',
+            'karma-typescript-preprocessor'
+        ],
+
+        // list of files to exclude
+        exclude: [],
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'app/**/*.ts': ['typescript']
+        },
+
+        typescriptPreprocessor: {
+            // options passed to the typescript compiler
+            options: {
+                sourceMap: true, // (optional) Generates corresponding .map file.
+                target: 'ES5', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5'
+                module: 'amd', // (optional) Specify module code generation: 'commonjs' or 'amd'
+                noImplicitAny: true, // (optional) Warn on expressions and declarations with an implied 'any' type.
+                noResolve: true, // (optional) Skip resolution and preprocessing.
+                removeComments: true, // (optional) Do not emit comments to output.
+                concatenateOutput: false // (optional) Concatenate and emit output to single file. By default true if module option is omited, otherwise false.
+            },
+            // extra typing definitions to pass to the compiler (globs allowed)
+            typings: [
+                'typings/tsd.d.ts'
+            ],
+            // transforming the filenames
+            transformPath: function(path) {
+                return path.replace(/\.ts$/, '.js');
+            }
+        },
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+        // web server port
+        port: 9876,
 
 
-    // web server port
-    port: 9876,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS'],
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true,
 
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity
+    })
 }
