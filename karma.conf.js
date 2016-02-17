93fbe1c4745c356bf12f0fee2ae677914ecdc87f
Pierre-Henri <phbasic@gmail.com>
2016-02-17 14:55:07 +0100

    Can't make tsc preprocessor to work

--- karma.conf.js
+++ karma.conf.js
@@ -32,8 +32,7 @@ module.exports = function(config) {
             'karma-phantomjs-launcher',
             'karma-jasmine',
             'karma-junit-reporter',
-            'karma-coverage',
-            'karma-typescript-preprocessor'
+            'karma-coverage'
         ],
 
         // list of files to exclude
@@ -42,28 +41,6 @@ module.exports = function(config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            'app/**/*.ts': ['typescript']
-        },
-
-        typescriptPreprocessor: {
-            // options passed to the typescript compiler
-            options: {
-                sourceMap: true, // (optional) Generates corresponding .map file.
-                target: 'ES5', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5'
-                module: 'amd', // (optional) Specify module code generation: 'commonjs' or 'amd'
-                noImplicitAny: true, // (optional) Warn on expressions and declarations with an implied 'any' type.
-                noResolve: true, // (optional) Skip resolution and preprocessing.
-                removeComments: true, // (optional) Do not emit comments to output.
-                concatenateOutput: false // (optional) Concatenate and emit output to single file. By default true if module option is omited, otherwise false.
-            },
-            // extra typing definitions to pass to the compiler (globs allowed)
-            typings: [
-                'typings/tsd.d.ts'
-            ],
-            // transforming the filenames
-            transformPath: function(path) {
-                return path.replace(/\.ts$/, '.js');
-            }
         },
 
         // test results reporter to use
