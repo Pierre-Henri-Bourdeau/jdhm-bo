1b96c9b3bbe2b30a711e244b88100720b4754f5e
Pierre-Henri <phbasic@gmail.com>
2016-02-17 11:17:59 +0100

    Karma is working

---
+++ app/class/client.class.spec.ts
@@ -0,0 +1,18 @@
+import {ClientClass} from './client.class';
+
+describe('1st tests', () => {
+
+    let client = new ClientClass('John', 'Smith', 'foo@bar.io');
+
+    it('FirstName should be set', () => {
+        expect(client.firstName).toEqual('John')
+    });
+
+    it('LastName should be set', () => {
+        expect(client.lastName).toEqual('Smith')
+    });
+
+    it('Email should be set', () => {
+        expect(client.email).toEqual('foo@bar.io')
+    });
+});
