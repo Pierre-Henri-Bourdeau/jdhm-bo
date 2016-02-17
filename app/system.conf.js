768914034b5b5e53212c2143d069ca5d172aa8e9
Pierre-Henri <phbasic@gmail.com>
2016-02-17 11:35:07 +0100

    Exclude system.conf.js

---
+++ app/system.conf.js
@@ -0,0 +1,21 @@
+System.config({
+    baseURL: '/',
+
+    // Set paths for third-party libraries as modules
+    paths: {
+        'angular': 'app/bower_components/angular/angular.js',
+        'angular-route': 'app/bower_components/angular-route/angular-route.js',
+        'babel': 'node_modules/babel-core/browser.js',
+        'es6-module-loader': 'node_modules/es6-module-loader/dist/es6-module-loader.js',
+        'systemjs': 'node_modules/systemjs/dist/system.js',
+        'system-polyfills': 'node_modules/systemjs/dist/system-polyfills.js'
+    },
+    packages: {
+        app: {
+            format: 'register',
+            defaultExtension: 'js'
+        }
+    },
+    // opt in to Babel for transpiling over Traceur
+    transpiler: 'babel'
+});
