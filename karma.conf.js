194bd304d353a912145361c2cb73e0a1c7c1a856
Pierre-Henri <pierre-henri.bourdeau@brandalley.com>
2016-02-19 12:07:37 +0100

    fixes #9

--- karma.conf.js
+++ karma.conf.js
@@ -41,12 +41,19 @@ module.exports = function(config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
+            'app/**/!(*spec).js': ['coverage']
         },
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress', 'coverage'],
+
+        coverageReporter: {
+            reporters:[
+                {type: 'json', subdir: '.', file: 'coverage-final.json'}
+            ]
+        },
 
 
         // web server port
