7ea6088107a21d85a52616bae664a77fdc9f47eb
Pierre-Henri <phbasic@gmail.com>
2016-05-29 13:48:15 +0200

    Typo. Test still not working

--- app/systemjs.config.js
+++ app/systemjs.config.js
@@ -14,7 +14,7 @@
       'typescript':         'node_modules/typescript/lib/typescript.js',
       'systemjs':           'node_modules/systemjs/dist/system.js',
       'system-polyfills':   'node_modules/systemjs/dist/system-polyfills.js',
-      'es6-module-loader':  'node_modules/es6-module-loader/dist/es6-module-loader.js'
+      'es6-module-loader':  'node_modules/es6-module-loader/dist/es6-module-loader.js',
       'phantomjs-polyfill': 'node_modules/phantomjs-polyfill/bind-polyfill.js'
   };
   // packages tells the System loader how to load when no filename and/or no extension
